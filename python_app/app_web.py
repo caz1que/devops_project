f9ec2726b61bd7e952732e4dfa6bb5fab3a74a3c
caz1que <coffin.for.puffin@gmail.com>
2023-05-07 19:28:19 +0300

    feat: python & mysql apps was maded

--- python_app/app_web.py
+++ python_app/app_web.py
@@ -1,20 +1,20 @@
 from flask import Flask, render_template
+import mysql.connector
+import os
 
 app = Flask(__name__)
 
-import mysql.connector
-
-connection = mysql.connector.connect(user='root', password='root', host='192.168.1.98', port="3306", database='db')
+connection = mysql.connector.connect(user='root', password=os.getenv('db_root_password'), host=os.getenv('MYSQL_SERVICE_HOST', port="3306", database='db')
 print("DB connected")
 
 cursor = connection.cursor()
 cursor.execute('Select * FROM Users')
-students = cursor.fetchall()
+users = cursor.fetchall()
 connection.close()
 
 @app.route('/')
 def index():
-    return render_template('index.html', students=students)
+    return render_template('index.html', users=users)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0", port="5000")
